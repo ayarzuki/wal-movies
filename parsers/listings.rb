6c9c2090d2267ea088c56e3ec7ee20d6253008eb
ayarzuki <aulya.yarzuki@gmail.com>
2022-03-10 01:37:03 +0700

    try to adding headers and seeders and listing scripst version 1.0

---
+++ parsers/listings.rb
@@ -0,0 +1,78 @@
+html = Nokogiri.HTML(content)
+
+# p html
+products = html.css('div[data-stack-index="0"] section div.flex.flex-wrap div.mb1.ph1.pa0-xl')
+# p products
+products.each do |product|
+  href = product.css('a.absolute.w-100.h-100.z-1')['href'].text
+#   p href
+  url = URI.join('https://www.walmart.com, href').to_s
+  pages << {
+      url: url,
+      page_type: 'products',
+      fetch_type: 'browser',
+      vars: {},
+      headers: {
+        "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
+        "Sec-CH-UA" => "'Not A;Brand';v='99', 'Chromium';v='96', 'Google Chrome';v='96'",
+        "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding" => "gzip, deflate, br",
+        "Accept-Language" => "en-US,en;q=0.9,id;q=0.8",
+        "Sec-Ch-Ua-Arch" => "x86",
+        "Sec-Ch-Ua-Platform" => "Windows"
+      },
+      driver: {
+        pre_code: "await page.setViewport({
+            width: 1920 + Math.floor(Math.random() * 100),
+            height: 3000 + Math.floor(Math.random() * 100),
+            deviceScaleFactor: 1,
+            hasTouch: false,
+            isLandscape: false,
+            isMobile: false,
+        });",
+        disable_adblocker: true,
+      }
+    }
+end
+
+# pagination_links = html.css('.list.flex.items-center.justify-center.pa0 li')
+# next_ur = pagination_links ? pagination_links.css('a')[4].text.to_i : 0
+next_ur = html.css('.list.flex.items-center.justify-center.pa0 li a[aria-label="Next Page"] > @href')
+
+if next_ur != []
+  url = "https://www.walmart.com#{next_ur}"
+  pages << {
+      url: url,
+      page_type: 'listings',
+      fetch_type: 'browser',
+      headers: {
+        "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
+        "Sec-CH-UA" => "'Not A;Brand';v='99', 'Chromium';v='96', 'Google Chrome';v='96'",
+        "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding" => "gzip, deflate, br",
+        "Accept-Language" => "en-US,en;q=0.9,id;q=0.8",
+        "Sec-Ch-Ua-Arch" => "x86",
+        "Sec-Ch-Ua-Platform" => "Windows"
+      },
+      driver: {
+        code: "await page.click(iframe[style='display: block; width: 100%; height: 100px; border: 0; -moz-user-select: none; -khtml-user-select: none; -webkit-user-select: none; -ms-user-select: none; user-select: none;'], {delay: 10000});",
+        pre_code: "await page.setViewport({
+            width: 1920 + Math.floor(Math.random() * 100),
+            height: 3000 + Math.floor(Math.random() * 100),
+            deviceScaleFactor: 1,
+            hasTouch: false,
+            isLandscape: false,
+            isMobile: false,
+        });",
+        disable_adblocker: true,
+      },
+      screenshot: {
+        "take_screenshot": true,
+        "options": {
+         "fullPage": false,
+         "type": "jpeg",
+         "quality": 75
+       }
+    }
+    }
+end
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,17 +2,34 @@ pages << {
     page_type: "listings",
     method: "GET",
     headers: {
-        "User-Agent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
-        "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "sec-ch-ua" => '"Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
-        "sec-ch-ua-mobile"=> "?0",
-        "sec-ch-ua-platform"=> '"Linux"',
-        "sec-fetch-dest"=> "document",
-        "sec-fetch-mode"=> "navigate",
-        "sec-fetch-site" => "same-origin",
-        "sec-fetch-user" => "?1",
-        "upgrade-insecure-requests" => "1"
+        "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
+        "Sec-CH-UA" => "'Not A;Brand';v='99', 'Chromium';v='96', 'Google Chrome';v='96'",
+        "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding" => "gzip, deflate, br",
+        "Accept-Language" => "en-US,en;q=0.9,id;q=0.8",
+        "Sec-Ch-Ua-Arch" => "x86",
+        "Sec-Ch-Ua-Platform" => "Windows"
     },
     url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
     fetch_type: "browser",
+    driver: {
+        pre_code: "await page.setViewport({
+            width: 1920 + Math.floor(Math.random() * 100),
+            height: 3000 + Math.floor(Math.random() * 100),
+            deviceScaleFactor: 1,
+            hasTouch: false,
+            isLandscape: false,
+            isMobile: false,
+        });",
+        code: "await page.click(iframe[style='display: block; width: 100%; height: 100px; border: 0; -moz-user-select: none; -khtml-user-select: none; -webkit-user-select: none; -ms-user-select: none; user-select: none;'], {delay: 10000});",
+        disable_adblocker: true,
+    },
+    screenshot: {
+        "take_screenshot": true,
+        "options": {
+         "fullPage": false,
+         "type": "jpeg",
+         "quality": 75
+       }
+    }
 }
