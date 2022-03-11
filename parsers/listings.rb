291764347fba2547e38d0f5ad295a3dfe8244c13
ayarzuki <aulya.yarzuki@gmail.com>
2022-03-11 22:08:30 +0700

    fixing parsers listings script file within adding driver code

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -1,5 +1,7 @@
 html = Nokogiri.HTML(content)
 
+# puts html
+
 products = html.css('div.flex-wrap div.mb1.ph1')
 
 products.each do |product|
@@ -12,16 +14,31 @@ products.each do |product|
             method: "GET",
             headers: {
                 "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "sec-ch-ua" => '"Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
-                "sec-ch-ua-mobile"=> "?0",
-                "sec-ch-ua-platform"=> '"Linux"',
-                "sec-fetch-dest"=> "document",
-                "sec-fetch-mode"=> "navigate",
-                "sec-fetch-site" => "same-origin",
-                "sec-fetch-user" => "?1",
+                "accept-encoding" => "gzip",
+                "content-security-policy" => "policy",
+                "sec-ch-ua" => '"Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+                "sec-ch-ua-mobile" => "?0",
+                "sec-ch-ua-full-version-list" => 'Not A;Brand";v="99.0.0.0", "Chromium";v="99.0.4844.51", "Google Chrome";v="99.0.4844.51"',
+                "sec-ch-ua-platform" => '"Linux"',
+                "sec-fetch-dest" => "image",
+                "sec-fetch-mode" => "no-cors",
+                "sec-fetch-site" => "cross-site",
                 "upgrade-insecure-requests" => "1",
+                "content-type" => "application/javascript; charset=utf-8",
             },
             fetch_type: "browser",
+            http2: true,
+            force_fetch: true,
+            ua_type: "none",
+            display: {
+                "width": 1280,
+                "height": 695
+            },
+            driver: { 
+                "name": "scraping walmart",
+                "stealth": true,
+                "enbable_images": false,
+            }
         }
     end
 end
@@ -48,6 +65,18 @@ page_links.each do |link|
                 "upgrade-insecure-requests" => "1",
             },
             fetch_type: "browser",
+            http2: true,
+            force_fetch: true,
+            ua_type: "none",
+            display: {
+                "width": 1280,
+                "height": 695
+            },
+            driver: { 
+                "name": "scraping walmart",
+                "stealth": true,
+                "enbable_images": false,
+            }
         }
     end
 end
