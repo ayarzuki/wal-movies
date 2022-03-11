bee76a6a342051cc9c40921f40928d0bc8cf534f
ayarzuki <aulya.yarzuki@gmail.com>
2022-03-11 21:15:32 +0700

    updating seeder file

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -1,82 +1,57 @@
 html = Nokogiri.HTML(content)
 
 # p html
+
 products = html.css('div[data-stack-index="0"] section div.flex.flex-wrap div.mb1.ph1.pa0-xl')
-# p products
-products.each do |product|
-  href = product.css('a.absolute.w-100.h-100.z-1')['href'].text
-#   p href
-  url = URI.join('https://www.walmart.com, href').to_s
-  pages << {
-      url: url,
-      page_type: 'products',
-      fetch_type: 'fullbrowser',
-      vars: {},
-      headers: {
-        "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "sec-ch-ua" => '"Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
-        "sec-ch-ua-mobile"=> "?0",
-        "sec-ch-ua-platform"=> '"Linux"',
-        "sec-fetch-dest"=> "document",
-        "sec-fetch-mode"=> "navigate",
-        "sec-fetch-site" => "same-origin",
-        "sec-fetch-user" => "?1",
-        "upgrade-insecure-requests" => "1",
-      },
-      driver: {
-        pre_code: "await page.setViewport({
-            width: 1920 + Math.floor(Math.random() * 100),
-            height: 3000 + Math.floor(Math.random() * 100),
-            deviceScaleFactor: 1,
-            hasTouch: false,
-            isLandscape: false,
-            isMobile: false,
-        });",
-        disable_adblocker: true,
-      }
-    }
-end
 
-# pagination_links = html.css('.list.flex.items-center.justify-center.pa0 li')
-# next_ur = pagination_links ? pagination_links.css('a')[4].text.to_i : 0
-next_ur = html.css('.list.flex.items-center.justify-center.pa0 li a[aria-label="Next Page"] > @href')
+p products
 
-if next_ur != []
-  url = "https://www.walmart.com#{next_ur}"
-  pages << {
-      url: url,
-      page_type: 'listings',
-      fetch_type: 'fullbrowser',
-      headers: {
-        "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "sec-ch-ua" => '"Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
-        "sec-ch-ua-mobile"=> "?0",
-        "sec-ch-ua-platform"=> '"Linux"',
-        "sec-fetch-dest"=> "document",
-        "sec-fetch-mode"=> "navigate",
-        "sec-fetch-site" => "same-origin",
-        "sec-fetch-user" => "?1",
-        "upgrade-insecure-requests" => "1",
-      },
-      driver: {
-        code: "await page.click(iframe[style='display: block; width: 100%; height: 100px; border: 0; -moz-user-select: none; -khtml-user-select: none; -webkit-user-select: none; -ms-user-select: none; user-select: none;'], {delay: 10000});",
-        pre_code: "await page.setViewport({
-            width: 1920 + Math.floor(Math.random() * 100),
-            height: 3000 + Math.floor(Math.random() * 100),
-            deviceScaleFactor: 1,
-            hasTouch: false,
-            isLandscape: false,
-            isMobile: false,
-        });",
-        disable_adblocker: true,
-      },
-      screenshot: {
-        "take_screenshot": true,
-        "options": {
-         "fullPage": false,
-         "type": "jpeg",
-         "quality": 75
-       }
-    }
-    }
-end
+# products.each do |product|
+#     url = product.at_css('a')['href']
+#     if url
+#         urlfull = URI.join('https://www.walmart.com', url).to_s
+#         pages << {
+#             url: urlfull,
+#             page_type: 'products',
+#             method: "GET",
+#             headers: {
+#                 "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+#                 "sec-ch-ua" => '"Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+#                 "sec-ch-ua-mobile"=> "?0",
+#                 "sec-ch-ua-platform"=> '"Linux"',
+#                 "sec-fetch-dest"=> "document",
+#                 "sec-fetch-mode"=> "navigate",
+#                 "sec-fetch-site" => "same-origin",
+#                 "sec-fetch-user" => "?1",
+#                 "upgrade-insecure-requests" => "1"
+#             },
+#             fetch_type: "fullbrowser",
+#         }
+#     end
+# end
+
+# page_links = html.css('[aria-label="pagination"] li a')
+
+# page_links.each do |link|
+#     url = link['href']
+#     if url
+#         urlfull = URI.join('https://www.walmart.com', url).to_s
+#         pages << {
+#             url: urlfull,
+#             page_type: 'listings',
+#             method: "GET",
+#             headers: {
+#                 "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+#                 "sec-ch-ua" => '"Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+#                 "sec-ch-ua-mobile"=> "?0",
+#                 "sec-ch-ua-platform"=> '"Linux"',
+#                 "sec-fetch-dest"=> "document",
+#                 "sec-fetch-mode"=> "navigate",
+#                 "sec-fetch-site" => "same-origin",
+#                 "sec-fetch-user" => "?1",
+#                 "upgrade-insecure-requests" => "1"
+#             },
+#             fetch_type: "fullbrowser",
+#         }
+#     end
+# end
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,36 +2,26 @@ pages << {
     page_type: "listings",
     method: "GET",
     headers: {
-        "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-encoding" => "gzip",
+        "content-security-policy" => "policy",
         "sec-ch-ua" => '"Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
-        "sec-ch-ua-mobile"=> "?0",
-        "sec-ch-ua-platform"=> '"Linux"',
-        "sec-fetch-dest"=> "document",
-        "sec-fetch-mode"=> "navigate",
-        "sec-fetch-site" => "same-origin",
-        "sec-fetch-user" => "?1",
+        "sec-ch-ua-mobile" => "?0",
+        "sec-ch-ua-full-version-list" => 'Not A;Brand";v="99.0.0.0", "Chromium";v="99.0.4844.51", "Google Chrome";v="99.0.4844.51"',
+        "sec-ch-ua-platform" => '"Linux"',
+        "sec-fetch-dest" => "image",
+        "sec-fetch-mode" => "no-cors",
+        "sec-fetch-site" => "cross-site",
         "upgrade-insecure-requests" => "1",
+        "content-type" => "application/javascript; charset=utf-8",
     },
     url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
-    fetch_type: "fullbrowser",
-    driver: {
-        pre_code: "await page.setViewport({
-            width: 1920 + Math.floor(Math.random() * 100),
-            height: 3000 + Math.floor(Math.random() * 100),
-            deviceScaleFactor: 1,
-            hasTouch: false,
-            isLandscape: false,
-            isMobile: false,
-        });",
-        code: "await page.click(iframe[style='display: block; width: 100%; height: 100px; border: 0; -moz-user-select: none; -khtml-user-select: none; -webkit-user-select: none; -ms-user-select: none; user-select: none;'], {delay: 10000});",
-        disable_adblocker: true,
-    },
-    screenshot: {
-        "take_screenshot": true,
-        "options": {
-         "fullPage": false,
-         "type": "jpeg",
-         "quality": 75
-       }
+    ua_type: "none",
+    fetch_type: "browser",
+    force_fetch: true,
+    http2: true,
+    display: {
+        "width": 1280,
+        "height": 695
     }
 }
